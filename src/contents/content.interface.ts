b2ef60065900e6119a58cb5c4e1b7eda61115f61
SnO2WMaN <me@sno2wman.net>
2021-10-04 00:55:14 +0900

    Content renaming

--- src/contents/content.interface.ts
+++ src/contents/content.interface.ts
@@ -2,7 +2,7 @@ import {Field, ID, InterfaceType} from '@nestjs/graphql';
 
 export type ContentType = 'BOOK' | 'BOOK_SERIES';
 
-@InterfaceType()
+@InterfaceType('Content')
 export abstract class IContent {
   @Field((type) => ID)
   id!: string;
