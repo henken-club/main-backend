8d5ebb3a2bf45fffe07925086d7115270c3c0d9f
SnO₂WMaN <me@sno2wman.net>
2021-10-06 20:30:29 +0900

    fix: ContentUnionにAuthorが入っていなかったので修正 (#38)

--- src/contents/content.union.ts
+++ src/contents/content.union.ts
@@ -3,10 +3,11 @@ import {createUnionType} from '@nestjs/graphql';
 import {ContentType} from './content.interface';
 import {BookEntity} from './books/book.entity';
 import {BookSeriesEntity} from './bookseries/bookseries.entity';
+import {AuthorEntity} from './authors/author.entity';
 
 export const ContentUnion = createUnionType({
   name: 'ContentUnion',
-  types: () => [BookEntity, BookSeriesEntity],
+  types: () => [BookEntity, BookSeriesEntity, AuthorEntity],
   resolveType(value: {type: ContentType}) {
     switch (value.type) {
       case 'BOOK':
@@ -14,7 +15,7 @@ export const ContentUnion = createUnionType({
       case 'BOOK_SERIES':
         return BookSeriesEntity;
       case 'AUTHOR':
-        return BookSeriesEntity;
+        return AuthorEntity;
     }
     return null;
   },
